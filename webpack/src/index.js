e0910d9c209d8e58fe6baf724f35d2b803138a5c
shunzizhan <shunzizhan@163.com>
2017-11-13 09:21:23 +0800

    20171113

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -21,13 +21,13 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-	element.classList.add('hello');
+  element.classList.add('hello');
 
-	   // 将图像添加到我们现有的 div。
-   var myIcon = new Image();
-   myIcon.src = Icon;
+  // 将图像添加到我们现有的 div。
+  var myIcon = new Image();
+  myIcon.src = Icon;
 
-   element.appendChild(myIcon);
+  element.appendChild(myIcon);
   return element;
 }
 
